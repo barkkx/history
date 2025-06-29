9a5e6526688a9b529f6a152d4a76ca57c660744a
barkkx <MUBARAKSIND52@GMAIL.COM>
2025-06-29 15:31:24 +0500

    История создания первого компьютера

---
+++ main.py
@@ -0,0 +1,26 @@
+import pdfplumber
+
+from gtts import gTTS
+
+
+def get_pdf_text(pdf_file):
+    with pdfplumber.PDF(open(pdf_file, mode='rb')) as f:
+        pages = f.pages
+        pdf_text = ''.join(page.extract_text() for page in pages)
+    return pdf_text.replace("\n", ' ')
+
+
+def get_media_file(pdf_text):
+    language = 'ru'
+    phrase = gTTS(text=pdf_text, lang=language, slow=False)
+    phrase.save("text.mp3")
+
+
+def main():
+    file_path = "Text.pdf"
+    pdf_text = get_pdf_text(file_path)
+    get_media_file(pdf_text)
+
+
+if __name__ == '__main__':
+    main()
